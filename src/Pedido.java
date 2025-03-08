0715580e99cda2d39e792e6db8d478f6cea481d2
jesusturpin <jesus.turpin@murciaeduca.es>
2025-03-08 10:00:20 +0100

    Refactorización 2: unificando procesarPedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -12,21 +12,8 @@ public class Pedido {
     }
 
     public void procesarPedido(double total) {
-        if (total > 100) {
-            double descuento = total * 0.10;
-            double precioFinal = total - descuento;
-            System.out.println("Pedido aprobado. Total: " + precioFinal + " (descuento aplicado: " + descuento + ")");
-        } else {
-            System.out.println("Pedido aprobado. Total: " + total);
-        }
-    }
-    public void procesarPedidoVIP(double total) {
-        if (total > 100) {
-            double descuento = total * 0.15;
-            double precioFinal = total - descuento;
-            System.out.println("Pedido VIP aprobado. Total: " + precioFinal + " (descuento aplicado: " + descuento + ")");
-        } else {
-            System.out.println("Pedido VIP aprobado. Total: " + total);
-        }
+        double descuento = total > 100 ? total * (cliente.isEsVip() ? 0.15 : 0.10) : 0;
+        double precioFinal = total - descuento;
+        System.out.println("Pedido " + (cliente.isEsVip() ? "VIP " : "") + "aprobado. Total: " + precioFinal + " (descuento aplicado: " + descuento + ")");
     }
 }
